9f5b7b9b01ff735ec4fb5ce03d3f60b60027ebb9
CyberZest <vijay.udgroups@gmail.com>
2023-04-13 12:00:04 +0530

    Update Linux_setup.sh

--- Linux_setup.sh
+++ Linux_setup.sh
@@ -22,10 +22,12 @@ if ! command -v javac >/dev/null 2>&1; then
     echo "Java JDK 19 is not installed, installing"
     mkdir -p /usr/local/java
     curl -L https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz -o jdk19.tar.gz
-    tar -xf jdk19.tar.gz -C /usr/local/java/ --strip-components=1
+    tar -xf jdk19.tar.gz -C /usr/local/java/jdk19 --strip-components=1
     rm jdk19.tar.gz
-    echo "export JAVA_HOME=/usr/local/java/" | sudo tee -a /etc/profile
-    echo "export PATH=\$PATH:\$JAVA_HOME/bin" | sudo tee -a /etc/profile
+    echo "export JAVA_HOME=/usr/local/java/jdk19" | sudo tee -a /etc/environment
+    echo "export PATH=\$PATH:\$JAVA_HOME/bin" | sudo tee -a /etc/environment
+    sudo update-alternatives --install /usr/bin/java java /usr/local/java/jdk19/bin/java 1
+    sudo update-alternatives --install /usr/bin/javac javac /usr/local/java/jdk19/bin/javac 1
     source /etc/profile
     echo "Java JDK 19 downloaded and installed successfully"
 fi
@@ -39,6 +41,8 @@ if ! command -v java >/dev/null 2>&1; then
     sudo rm /usr/local/java/jre8/jre8.tar.gz
     sudo update-alternatives --install /usr/bin/java java /usr/local/java/jre8/jre1.8.0_301/bin/java 1
     sudo update-alternatives --install /usr/bin/javac javac /usr/local/java/jre8/jre1.8.0_301/bin/javac 1
+    sudo update-alternatives --set java /usr/local/java/jre8/jre1.8.0_301/bin/java 1
+    sudo update-alternatives --set javac /usr/local/java/jre8/jre1.8.0_301/bin/javac 1
     echo "Java JRE 8 downloaded and installed successfully"
 fi
 
